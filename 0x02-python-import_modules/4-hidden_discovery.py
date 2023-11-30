5f113a8bf353d3bbee1fabb34fd4faf7d29dcc77
Mayad95 <mayadhammour95@gmail.com>
2023-11-30 13:04:03 -0800

    first

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """print all hidden directories"""
     import hidden_4
-    
+
     for i in dir(hidden_4):
-        if i [:2] != "__":
-        print(i)
+        if i[:2] != "__":
+            print(i)
